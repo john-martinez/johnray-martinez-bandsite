26c2ccc8d15fa49d5e352c54c6232475a7a229af
johnray-martinez <jrvmartinez@icloud.com>
2020-02-01 11:51:23 -0500

    implemented dynamic time stamp function

--- sprint-2/scripts/main.js
+++ sprint-2/scripts/main.js
@@ -2,20 +2,17 @@
   const comments = [{
     name: 'Michael Lyons',
     timeStamp: '12/18/2018',
-    comment:'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.',
-    picture: ''
+    comment:'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
   }, 
   {
     name: 'Gary Wong',
     timeStamp: '12/12/2018',
-    comment: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!',
-    picture: ''
+    comment: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!'
   },
   {
     name: 'Theodore Duncan',
     timeStamp: '11/15/2018',
-    comment: 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!',
-    picture: ''
+    comment: 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!'
   }];
   
 
@@ -48,7 +45,7 @@ const displayComment = comments => {
         commentHeader.appendChild(commentAuthor);
         let commentTimeStamp = document.createElement('span');
         commentTimeStamp.classList.add('comment__time-stamp');
-        commentTimeStamp.innerText = item.timeStamp;
+        commentTimeStamp.innerText = dynamicTimeStamp(item.timeStamp);
         commentHeader.appendChild(commentTimeStamp);
         commentRight.appendChild(commentHeader);
         let commentBlurb = document.createElement('div');
@@ -83,31 +80,34 @@ document.querySelector('.form').addEventListener('submit', e => {
 // dynamic time stamp
 const dynamicTimeStamp = timestamp => {
   const time = [  
-  { unit: 'seconds', divider: 60 },
-  { unit: 'minutes', divider: 60 },
-  { unit: 'hours',   divider: 24 },
-  { unit: 'days',    divider: 7  },
-  { unit: 'weeks',   divider: 4  },
-  { unit: 'months',  divider: 12  }];
+  { unit: 'sec', divider: 60 },
+  { unit: 'min', divider: 60 },
+  { unit: 'hr',   divider: 24 },
+  { unit: 'day',    divider: 7  },
+  { unit: 'wk',   divider: 4  },
+  { unit: 'mon',  divider: 12  }];
   let toConvert = new Date(timestamp);
   let rightNow = new Date(Date.now());
-  let res = Math.abs(toConvert - rightNow); // get the difference of the two dates in milliseconds
+  let res = rightNow - toConvert; // get the difference of the two dates in milliseconds
   res /= 1000; // convert milliseconds to seconds
-  // ${<unit>!= 1?'s':''}  <--- ternary operator to add 's' if the res is > 1
+ 
 
-  if (res > 0){
+  if (res >= 1){
     for(let i = 0; i < time.length; i++){
-      if (i == time.length-1){
-        // if the difference between year from timestamp and is <= 0 go to next if. Else, output result in years
-        let year = Math.abs(toConvert.getFullYear() - rightNow.getFullYear());
-        if (year <= 0){
-          // subtract month from rightNow to toConvert and output result in months
-          let month = Math.abs((toConvert.getMonth()+1) - (rightNow.getMonth()+1)); // +1 because getMonth() returns a value from 0-11
-          return `${month} month${month!= 1?'s':''} ago`;
-        } else return `${year} year${year!= 1?'s':''} ago`;
+      // this block triggers if timestamp is more than or equal to 4 weeks
+      if (i == time.length-1){ 
+        // convert year to months (1year = 12 months) and add the rest of the months to the result
+        // add toConvert month and rightNow month to their respective results
+        let rightNowMonths = (rightNow.getFullYear()*12) + (rightNow.getMonth()+1); 
+        let toConvertMonths = (toConvert.getFullYear()*12) + (toConvert.getMonth()+1);
+        // subtract the results and you will get the results in months format
+        res = rightNowMonths - toConvertMonths; 
+        if (res >= 12) return `${Math.floor(res/12)} yr${(Math.floor(res/12)>=2)?'s':''} ago`;
+        else return `${res} mon${res!= 1?'s':''} ago`;
       }
       else  {
-        if (res < time[i].divider) return `${Math.round(res)} ${time[i].unit}${res!= 1?'s':''} ago`;
+        // cycle through the time array and divide res by time[i].divider
+        if (res < time[i].divider) return `${Math.round(res)} ${time[i].unit}${res!= 1?'s':''} ago`;  // ${<unit>!= 1?'s':''}  <--- ternary operator to add 's' if the res is > 1
         else res /= time[i].divider;
       }
     }
